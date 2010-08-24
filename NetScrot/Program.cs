be0dc7fe47a5b0426a98e380e0f299e96f4ff611
mudtail <maff130@gmail.com>
2010-08-25 00:49:26 +0100

    Initial commit to .net version

---
+++ NetScrot/Program.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+using System.Collections;
+using System.Collections.Specialized;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Runtime.InteropServices;
+
+namespace NetScrot {
+	static class Program {
+		/// <summary>
+		/// The main entry point for the application.
+		/// </summary>
+		private static netscrot nscrot = new netscrot();
+
+		private static bool kprtscrn = false;
+		private static bool klctrl = false;
+		private static bool klshift = false;
+#if USE_ALT_METHOD
+        private static bool kalt = false;
+#endif
+
+		/// <summary>The GetForegroundWindow function returns a handle to the foreground window.</summary>
+#if USE_ALT_METHOD
+        [DllImport("user32.dll")]static extern IntPtr GetForegroundWindow( );
+        [DllImport("gdi32.dll")]static extern bool GetWindowOrgEx(IntPtr hdc, out Point lpPoint);
+#endif
+
+		[STAThread]
+		static void Main( ) {
+			Application.EnableVisualStyles();
+			Application.SetCompatibleTextRenderingDefault(false);
+			nscrot.hook.HookedKeys.Add(Keys.PrintScreen);
+			nscrot.hook.HookedKeys.Add(Keys.LControlKey);
+			nscrot.hook.HookedKeys.Add(Keys.LShiftKey);
+			nscrot.hook.KeyDown += new KeyEventHandler(hook_KeyDown);
+			nscrot.hook.KeyUp += new KeyEventHandler(hook_KeyUp);
+			nscrot.init();
+			Application.Run();
+		}
+
+		public static void runUpload( ) {
+			if (Properties.Settings.Default.firstrun)
+				return;
+			NameValueCollection postdata = new NameValueCollection();
+			postdata.Add("u", Properties.Settings.Default.username);
+			postdata.Add("p", Properties.Settings.Default.password);
+			if (!Clipboard.ContainsImage()) {
+				nscrot.balloonText("No image in clipboard!", 2);
+				return;
+			}
+			Image scrt = Clipboard.GetImage();
+			System.Threading.Thread upld = new System.Threading.Thread(( ) =>
+			{
+				nscrot.upload(postdata, scrt);
+			});
+			upld.SetApartmentState(System.Threading.ApartmentState.STA);
+			upld.Start();
+		}
+
+		private static void hook_KeyUp(object sender, KeyEventArgs e) {
+			kprtscrn = false;
+			klctrl = false;
+			klshift = false;
+		}
+
+		private static void hook_KeyDown(object sender, KeyEventArgs e) {
+			if (Properties.Settings.Default.firstrun)
+				return;
+			if (e.KeyCode == Keys.PrintScreen)
+				kprtscrn = true;
+			if (e.KeyCode == Keys.LShiftKey)
+				klshift = true;
+			if (e.KeyCode == Keys.LControlKey)
+				klctrl = true;
+			if (kprtscrn && klctrl && klshift) {
+				NameValueCollection postdata = new NameValueCollection();
+				postdata.Add("u", Properties.Settings.Default.username);
+				postdata.Add("p", Properties.Settings.Default.password);
+#if USE_ALT_METHOD
+                Graphics scrot = null;
+                Image scrt = null;
+                
+                switch (kalt) {
+                    case false:
+                        Rectangle tsize = Rectangle.Empty;
+                        foreach (Screen s in Screen.AllScreens)
+                            tsize = Rectangle.Union(tsize, s.Bounds);
+                        scrt = new Bitmap(tsize.Width, tsize.Height, System.Drawing.Imaging.PixelFormat.Format32bppArgb);
+                        scrot = Graphics.FromImage(scrt);
+                        scrot.CopyFromScreen(tsize.X, tsize.Y, 0, 0, tsize.Size, CopyPixelOperation.SourceCopy);
+                        break;
+                    case true:
+                        IntPtr awHnd = GetForegroundWindow();
+                        Point wndPts = Point.Empty;
+                        bool diditwork = GetWindowOrgEx(awHnd, out wndPts);
+                        scrt = new Bitmap(wndPts.X, wndPts.Y, System.Drawing.Imaging.PixelFormat.Format32bppArgb);
+                        scrot = Graphics.FromImage(scrt);
+                        scrot.CopyFromScreen(wndPts.X, wndPts.Y, 0, 0, new Size(wndPts.X, wndPts.Y), CopyPixelOperation.SourceCopy);
+                        break;
+
+                }
+#else
+				if (!Clipboard.ContainsImage()) {
+					nscrot.balloonText("No image in clipboard!", 2);
+					return;
+				}
+				Image scrt = Clipboard.GetImage();
+#endif
+				System.Threading.Thread upld = new System.Threading.Thread(( ) =>
+				{
+					nscrot.upload(postdata, scrt);
+				});
+				upld.SetApartmentState(System.Threading.ApartmentState.STA);
+				upld.Start();
+			}
+		}
+	}
+}
---
+++ NetScrot/frmUsrSettings.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace NetScrot {
+	public partial class frmUsrSettings : Form {
+		public frmUsrSettings( ) {
+			InitializeComponent();
+			this.KeyUp += new KeyEventHandler(keyup);
+			this.FormClosing += new FormClosingEventHandler(closeify);
+			this.Load += new EventHandler(loadify);
+			this.Icon = Properties.Resources.idle;
+		}
+
+		public void loadify(object sender, EventArgs e) {
+			txtUsername.Text = Properties.Settings.Default.username;
+			txtPass.Text = "Password";
+		}
+
+		public void closeify(object sender, FormClosingEventArgs e) {
+			if (Properties.Settings.Default.username.Length == 0 || Properties.Settings.Default.password.Length == 0 || Properties.Settings.Default.firstrun) {
+				e.Cancel = true;
+				MessageBox.Show("Please enter your login information.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+			} else {
+				this.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+				this.Dispose();
+			}
+		}
+
+		public void keyup(object sender, KeyEventArgs e) {
+			if (e.KeyCode == Keys.Enter && (!e.Alt && !e.Control && !e.Shift))
+				returnToSender();
+		}
+
+		private void btnSave_Click(object sender, EventArgs e) {
+			returnToSender();
+		}
+
+		private void returnToSender( ) {
+			if (txtUsername.Text.Length == 0) {
+				MessageBox.Show("Please enter your username.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+				return;
+			}
+			if (txtPass.Text == "Password" && Properties.Settings.Default.password.Length == 0) {
+				MessageBox.Show("Please enter your password.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+				return;
+			}
+			if (txtUsername.Text != Properties.Settings.Default.username)
+				Properties.Settings.Default.username = txtUsername.Text;
+			if (txtPass.Text != "Password" && txtPass.Text != Properties.Settings.Default.password)
+				Properties.Settings.Default.password = txtPass.Text;
+			if (Properties.Settings.Default.firstrun)
+				Properties.Settings.Default.firstrun = false;
+			Properties.Settings.Default.Save();
+			Properties.Settings.Default.Reload();
+			this.DialogResult = System.Windows.Forms.DialogResult.OK;
+			this.Dispose();
+		}
+	}
+}
