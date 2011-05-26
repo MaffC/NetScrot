0c654463cfa5a19ecd3fefede5311f44f05fc95b
Matthew Connelly <maff130@gmail.com>
2011-05-26 03:03:32 +0100

    Initial work on the settings class

--- nssettings.cpp
+++ nssettings.cpp
@@ -1,5 +1,11 @@
 #include "nssettings.h"
+#include <QFile>
+
+//Initialise netscrot settings class
+nsConf settings;
 
 nsSettings::nsSettings() {
-
+	//Load settings from file
+	QFile nsConfFile("~/");
+	nsConfFile.open()
 }
